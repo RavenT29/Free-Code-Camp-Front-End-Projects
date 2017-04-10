a9f078984967fa6a2f1c87e2d1d4db350f043a19
Ana <ana.toma92@gmail.com>
2017-04-10 11:54:33 +0300

    Add files via upload

--- Simon Game/js/index.js
+++ Simon Game/js/index.js
@@ -1,72 +1,128 @@
 /*
 User Story: I am presented with a random series of button presses. X DONE
-
 User Story: Each time I input a series of button presses correctly, I see the same series of button presses but with an additional step. X DONE
-
 User Story: I hear a sound that corresponds to each button both when the series of button presses plays, and when I personally press a button.
-
 User Story: If I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
-
 User Story: I can see how many steps are in the current series of button presses.
-
 User Story: If I want to restart, I can hit a button to do so, and the game will return to a single step. X DONE
-
 User Story: I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
-
 User Story: I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
 */
 
 //  VARIABLE DECLARATIONS
 
 var audio1 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-  audio2 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-  audio3 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-  audio4 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-var computer = [],
-  user = [];
-var counter = 0;
-var strict = false;
+    audio2 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    audio3 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    audio4 = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+var sequence = [],
+    user = [];
+var roundNum = 1,
+    counter = 1;
+var strict = true;
+var i =0, moves;
+
 //  MAIN FUNCTIONS
 
-function random() {
-  return Math.floor(Math.random() * 4 + 1);
-}
-
 function reset() {
-  computer = [];
-  user = [];
-  counter=0;
+    sequence = [];
+    user = [];
+    roundNum = 1;
 }
 
-function computerTurn() {
-  computer = computer.push(random());
+function generateSequence() {
+    var i = 0;
+    while (i < 20) {
+        i++;
+        var aux = [Math.floor(Math.random() * 4 + 1)];
+        sequence = $.merge(sequence, aux);
+    }
+    console.log(sequence); //test
 }
 
-function userTurn() {
-  var aux = $(this).attr("value");
-  aux = [parseInt(aux)];
-  user = user.concat(aux);
+function round() {
+    var value = $(this).attr("value");
+    value = parseInt(value);
+    click(value);
+
+    if (user.length < roundNum) {
+
+        if (value === sequence[counter - 1]) {
+            user = $.merge(user, [value]);
+        } else if (value !== sequence[counter - 1]) {
+            if (strict) {
+                counter = 1;
+                console.log("You lost!");
+
+            } else {
+                counter = 1;
+                console.log("Animate again!");
+            }
+        }
+    }
+    if (user.length === roundNum && roundNum < 20) {
+
+        console.log("You won this round");
+        user = [];
+        roundNum++;
+        counter = 1;
+      animate();
+    } else if (user.length < roundNum) {
+        counter++;
+    } else if (user.length === roundNum && roundNum === 20) {
+        console.log("You won the game");
+    }
 }
-strict=false;
-computer=[1,3,2];
-user=[1,3,4];
-function strictCheck(){
-  if(user[user.length-1]!==computer[computer.length-1]&&strict===true)
-    {
-      console.log("You lost");//replace with html // losing function
-      reset();
-} else if (user[user.length-1]!==computer[computer.length-1]&&strict===false){
-    user.splice(user.length-1);
-  } else {
-    counter++;
-  }
+
+function animation(val) {
+    setTimeout(function() {
+        if (val === 1) {
+            audio1.play();
+        } else if (val === 2) {
+            audio2.play();
+        } else if (val === 3) {
+            audio3.play();
+        } else if (val === 4) {
+            audio4.play();
+        }
+    }, 1000);
 }
 
+function animate() {
+    var i = -1;
+    moves = setInterval(function() {
+        i++;
+        if (i >= roundNum) {
+        clearInterval(moves);
+        }
+        $("#id"+(sequence[i-1])).addClass('hover');
+  setTimeout(function(){
+      $("#id"+(sequence[i-1])).removeClass('hover');
+  }, 500);  
+        click(sequence[i-1]).play();
+    }, 800);
+}
 
+function click(val) {
+    if (val === 1) {
+        audio1.play();
+    } else if (val === 2) {
+        audio2.play();
+    } else if (val === 3) {
+        audio3.play();
+    } else if (val === 4) {
+        audio4.play();
+    }
 
+}
 
 //  DOCUMENT READY
 
 $(document).ready(function() {
-  $(".semiCircle").on("click", strictCheck());
+
+    generateSequence();
+    $(".semiCircle").on("click", round);
+
 }); //document ready
+
+//("audio"+"value").play();
